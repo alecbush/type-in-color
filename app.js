fbc8e47588be21a65df68a71508a52b5e4144d7c
Alec Bush <alexander.joseph.bush@gmail.com>
2020-04-30 08:34:56 -0400

    Revise color shift

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const app = new Vue({
             let r = b = g = (this.text.length > 0) ? 7 : 0;
             new TextEncoder().encode(this.text)
                 .forEach(val => {
-                    r = ((b * val) + r) % 256;
-                    g = ((r * val) + g) % 256;
-                    b = ((g * val) + b) % 256;
+                    r = (b + r + val) % 256;
+                    g = (r + g + val) % 256;
+                    b = (g + b + val) % 256;
                 });
             const toHex = val => val.toString(16).padStart(2, '0');
             this.hex = '#' + (toHex(r) + toHex(g) + toHex(b));
